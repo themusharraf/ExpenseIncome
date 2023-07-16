4dc10ce5434a633ed2a564bf3ca3f5c95e14bf9b
msi <meibrohimov@gmail.com>
2023-07-16 19:17:32 +0500

    Send Email Account Activation django rest framework

--- root/settings.py
+++ root/settings.py
@@ -101,8 +101,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'meibrohimov@gmail.com'
-EMAIL_HOST_PASSWORD = 'hebjvqwpfqobcqys'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
+from django.urls import reverse
 from rest_framework import generics, status
 from users.serializer import RegisterSerializer
 from rest_framework.response import Response
+from users.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from users.utils import Util
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class RegisterView(generics.GenericAPIView):
@@ -11,7 +16,22 @@ class RegisterView(generics.GenericAPIView):
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+
+        token = RefreshToken.for_user(user).access_token
+
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://' + current_site + relativeLink + "?token=" + str(token)
+        email_body = "Hi" + user.username + 'Use link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
 
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
